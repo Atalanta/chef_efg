b867f0e5380e2cff81f6144809b88776f02d41d2
Stephen Nelson-Smith <stephen@atalanta-systems.com>
2014-11-16 20:16:11 +0000

    Add: attributes for ssh and fail2ban
    The ssh config is currently set to use port 22, as the higher port
    will require firewall and ssh config reconfiguration.
    
    The fail2ban config for nginx request limiting is disabled as there
    are a couple of issues with it:
    
    - machines without nginx won't have the log, which will prevent
      fail2ban from starting
    - the action for the jail is embedded in a substitution by the
      upstream cookbook template, which is not valid, and thus also
      prevents the service from starting

--- attributes/base.rb
+++ attributes/base.rb
@@ -1,4 +1,35 @@
 default['authorization']['sudo']['groups'] = ['wheel']
 default['authorization']['sudo']['passwordless'] = true
 default['openssh']['server']['permit_root_login'] = 'no'
-default['openssh']['server']['port'] = '21322'
+#default['openssh']['server']['port'] = '21322'
+default['openssh']['server']['port'] = '22'
+
+default['fail2ban']['ignoreip'] = '127.0.0.1/8 192.168.1.0/24'
+default['fail2ban']['bantime'] = 1200
+default['fail2ban']['email'] = 'stephen@atalanta-systems.com'
+default['fail2ban']['action'] = 'action_mwl'
+
+default['fail2ban']['filters'] = {
+  'nginx-req-limit' => {
+        "failregex" => ['limiting requests, excess:.* by zone.*client: <HOST>'],
+        "ignoreregex" => []
+     },
+}
+
+default['fail2ban']['services'] = {
+  'ssh' => {
+        "enabled" => "true",
+        "port" => "ssh",
+        "filter" => "sshd",
+        "logpath" => node['fail2ban']['auth_log'],
+        "maxretry" => "6"
+     },
+  'nginx-req-limit' => {
+        "enabled" => "false",
+        "port" => "http,https",
+        "filter" => "nginx-req-limit",
+        "logpath" => "/var/log/nginx/*error.log",
+        "action" => 'iptables-multiport[name=ReqLimit, port="http,https", protocol=tcp]',
+        "maxretry" => "6"
+     }
+}
