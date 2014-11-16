b7b131a5850c79a62f67bb7b750e42a04156d87c
Stephen Nelson-Smith <stephen@atalanta-systems.com>
2014-11-16 13:45:13 +0000

    Add: Send reports weekly rather than daily
    The reports generated by accounting, logwatch and rootkit finders
    all should be sent weekly.

--- recipes/base.rb
+++ recipes/base.rb
@@ -18,5 +18,24 @@ end
 
 cookbook_file '/etc/sysctl.d/10-network-security.conf' do
   source 'network-security.conf'
+  notifies :run, 'execute[Update kernel config]'
+end
 
+%w{ chkrootkit rkhunter acct logwatch }.each { |pkg| package pkg }
+
+%w{ 00logwatch acct chkrootkit rkhunter }.each do |job|
+
+  file "/etc/cron.daily/#{job}" do
+    action :delete
+  end
+  
+  cookbook_file "/etc/cron.weekly/#{job}" do
+    source "#{job}-cron"
+    mode '0755'
+  end
+end
+
+cookbook_file "/etc/cron.weekly/pacct-report" do
+  source "pacct-report-cron"
+  mode '0755'
 end
