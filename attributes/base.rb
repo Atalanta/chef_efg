267709008970dbc09a80f6cebfea30a690314593
Stephen Nelson-Smith <stephen@atalanta-systems.com>
2014-11-16 10:43:26 +0000

    Add: SSH Hardening
    Do not permit root logins, and run ssh on a high port.
    
    Remember that this will mean you can't connect to your kitchen
    instance unless you modify the port forwarding rule!

--- attributes/base.rb
+++ attributes/base.rb
@@ -1,3 +1,4 @@
 default['authorization']['sudo']['groups'] = ['wheel']
 default['authorization']['sudo']['passwordless'] = true
-
+default['openssh']['server']['permit_root_login'] = 'no'
+default['openssh']['server']['port'] = '21322'
