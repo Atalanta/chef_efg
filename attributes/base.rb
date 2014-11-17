14bec5da453243d9fc9bb49fd3d13d46994f3462
Stephen Nelson-Smith <stephen@atalanta-systems.com>
2014-11-17 06:36:47 +0000

    Change: set default ssh port to 21322
    Skyscape NAT rule changed for bastion.  Nothing else is externally
    exposed.
    
    Will also need to update fail2ban.

--- attributes/base.rb
+++ attributes/base.rb
@@ -1,8 +1,7 @@
 default['authorization']['sudo']['groups'] = ['wheel']
 default['authorization']['sudo']['passwordless'] = true
 default['openssh']['server']['permit_root_login'] = 'no'
-#default['openssh']['server']['port'] = '21322'
-default['openssh']['server']['port'] = '22'
+default['openssh']['server']['port'] = '21322'
 
 default['fail2ban']['ignoreip'] = '127.0.0.1/8 192.168.1.0/24'
 default['fail2ban']['bantime'] = 1200
