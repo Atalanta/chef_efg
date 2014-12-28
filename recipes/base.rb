a9a6e162c42244e94a1805dcd29c74024fe8982a
Stephen Nelson-Smith <stephen@atalanta-systems.com>
2014-12-28 11:47:14 +0000

    Change: Set permissions for various files

--- recipes/base.rb
+++ recipes/base.rb
@@ -4,18 +4,7 @@ execute 'Save iptables' do
 end
 
 bash 'Basic Firewall' do
-  code <<-EOH
-  iptables -P INPUT ACCEPT
-  iptables -P OUTPUT ACCEPT
-  iptables -F
-  iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-  iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-  iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-  iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
-  iptables -A INPUT -p tcp --dport 3306 -j ACCEPT
-  iptables -A INPUT -p tcp --dport 443 -j ACCEPT
-  iptables -P INPUT DROP
-  EOH
+  code ruleset(node)
   notifies :run, 'execute[Save iptables]'
 end
 
@@ -29,6 +18,14 @@ users_manage 'bastion' do
   action [:remove, :create]
 end
 
+data_bag('users').each do |u|
+
+  directory "/home/#{u}" do
+    mode 0711
+  end
+  
+end
+
 include_recipe 'sudo'
 include_recipe 'openssh'
 include_recipe 'fail2ban'
@@ -37,6 +34,10 @@ include_recipe 'chef-client::delete_validation'
 execute 'Update kernel config' do
   command 'service procps start'
   action :nothing
+end
+
+cookbook_file '/etc/issue' do
+  source 'etc-issue'
 end
 
 cookbook_file '/etc/sysctl.d/10-network-security.conf' do
@@ -62,3 +63,117 @@ cookbook_file "/etc/cron.weekly/pacct-report" do
   source "pacct-report-cron"
   mode '0755'
 end
+
+cookbook_file '/etc/pam.d/common-password' do
+  source 'common-password'
+end
+
+file '/sbin/mount.nfs' do
+  mode '0755'
+end
+
+file '/usr/bin/pkexec' do
+  mode '0755'
+end
+
+file '/usr/sbin/postdrop' do
+  mode '0555'
+end
+
+file '/usr/sbin/postqueue' do
+  mode '0555'
+end
+
+file '/usr/lib/dbus-1.0/dbus-daemon-launch-helper' do
+  mode '0754'
+end
+
+file '/usr/lib/policykit-1/polkit-agent-helper-1' do
+  mode '0755'
+end
+
+file '/usr/lib/emacs/24.3/x86_64-linux-gnu/movemail' do
+  mode '0755'
+end
+
+file '/var/log/wtmp' do
+  mode '0660'
+end
+
+file '/var/log/dpkg.log.1' do
+  mode '0640'
+end
+
+file '/var/log/dpkg.log' do
+  mode '0640'
+end
+
+file '/var/log/unattended-upgrades/unattended-upgrades-shutdown.log' do
+  mode '0640'
+end
+
+file '/var/log/apt/history.log' do
+  mode '0640'
+end
+
+file '/var/log/apt/history.log.1.gz' do
+  mode '0640'
+end
+
+file '/var/log/lastlog' do
+  mode '0660'
+end
+
+file '/var/log/boot.log' do
+  mode '0640'
+end
+
+file '/var/log/wtmp.1' do
+  mode '0660'
+end
+
+file '/var/log/bootstrap.log' do
+  mode '0640'
+end
+
+file '/var/log/faillog' do
+  mode '0640'
+end
+
+file '/var/log/alternatives.log' do
+  mode '0640'
+end
+
+file '/var/log/installer/initial-status.gz' do
+  mode '0640'
+end
+
+file '/var/log/installer/lsb-release' do
+  mode '0640'
+end
+
+file '/var/log/installer/status' do
+  mode '0640'
+end
+
+file '/var/log/installer/hardware-summary' do
+  mode '0640'
+end
+
+file '/var/log/installer/media-info' do
+  mode '0640'
+end
+
+file '/var/log/nginx/error.log' do
+  mode '0640'
+end
+
+file '/var/log/alternatives.log.1' do
+  mode '0640'
+end
+
+file '/var/log/landscape/sysinfo.log' do
+  mode '0640'
+end
+
+
